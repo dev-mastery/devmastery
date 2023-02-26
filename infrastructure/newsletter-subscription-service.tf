26159a0b289bacd720fe5cd18d0742f79c6af974
Bill Sourour <bill.sourour@arcnovus.com>
2023-02-26 10:33:48 -0500

    initial api gateway config

---
+++ infrastructure/newsletter-subscription-service.tf
@@ -0,0 +1,27 @@
+###############################################################################
+# API GATEWAY
+###############################################################################
+
+resource "aws_api_gateway_rest_api" "newsletter_subscription_api" {
+  name        = "newsletter-subscription-api"
+  description = "A service for subscribing to newsletters"
+  body        = file("../newsletter-subscription/api-spec/newsletter-subscription-api.yaml")
+}
+
+resource "aws_api_gateway_deployment" "newsletter_subscription_api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.newsletter_subscription_api.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.newsletter_subscription_api.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "newsletter_subscription_api_stage" {
+  deployment_id = aws_api_gateway_deployment.newsletter_subscription_api_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.newsletter_subscription_api.id
+  stage_name    = env.environment
+}
