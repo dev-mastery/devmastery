47ab8eaad4d0b7bce455eb40d53087dbf2509b38
Bill Sourour <bill.sourour@arcnovus.com>
2023-02-28 13:06:54 -0500

    fix typo in region variable

--- infrastructure/newsletter-subscription-service.tf
+++ infrastructure/newsletter-subscription-service.tf
@@ -67,5 +67,5 @@ resource "aws_lambda_permission" "api_gateway_invoke_lambda_permission" {
   action        = "lambda:InvokeFunction"
   function_name = module.lambda_create_newsletter_subscription.lambda_function_arn
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${var.region}:${var.aws_account_id}:${aws_api_gateway_rest_api.newsletter_subscription_api.id}/*/*"
+  source_arn    = "arn:aws:execute-api:${var.aws_region}:${var.aws_account_id}:${aws_api_gateway_rest_api.newsletter_subscription_api.id}/*/*"
 }
