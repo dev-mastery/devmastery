e27b4e6a48b8f06d9dc2b7ea49006874b4159086
Bill Sourour <bill.sourour@arcnovus.com>
2023-02-28 09:51:27 -0500

    add some logging

--- infrastructure/lambda/http/index.js
+++ infrastructure/lambda/http/index.js
@@ -1,4 +1,5 @@
 module.exports.handler = async (event) => {
+  console.log("Event: ", event);
   return {
     statusCode: 200,
     body: "Congrats! You've successfully deployed a Lambda function!",
