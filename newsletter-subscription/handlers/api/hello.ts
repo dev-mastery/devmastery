9b1a8d18e7d707feb426b3728a5d3c86752a61f2
Bill Sourour <bill.sourour@arcnovus.com>
2023-03-04 23:58:48 -0500

    publish event test

--- newsletter-subscription/handlers/api/hello.ts
+++ newsletter-subscription/handlers/api/hello.ts
@@ -1,9 +1,20 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import type { ApplicationEvent } from "../../../lib/event-publisher/src/";
+import { publishEvent } from "../../../lib/event-publisher/src/";
 
-export default function handler(
+export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
-  const { name } = request.query;
-  return response.end(`Hello ${name}!`);
+  let event: ApplicationEvent = {
+    id: "123",
+    type: "hello",
+    data: {
+      msg: `Hello from ${request.url}`,
+    },
+    topic: "hello",
+    createdAt: new Date(),
+  };
+  let result = await publishEvent(event);
+  response.status(200).json(result);
 }
