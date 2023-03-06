7f8cfd112568789604bf50cfcf76ddd553aef653
Bill Sourour <bill.sourour@arcnovus.com>
2023-03-05 21:14:02 -0500

    use folders for routing

--- newsletter-subscription/handlers/api/newsletter-subscriptions/index.ts
+++ newsletter-subscription/handlers/api/newsletter-subscriptions/index.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { NewsletterSubscriptionDecoder } from "@devmastery/newsletter-subscription-api/decoders";
-import { requestNewsletterSubscription } from "../use-cases/request-newsletter-subscription";
+import { requestNewsletterSubscription } from "../../use-cases/request-newsletter-subscription";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   switch (req.method) {
